b083b197fd3072a1658c60d625b36bb9cd763659
Khrystyna2409 <xrustuna.di@gmail.com>
2022-05-07 22:24:37 +0300

    array2

--- task01-arrays.go
+++ task01-arrays.go
@@ -11,7 +11,7 @@ func average(input [15]float32) (result float32) {
 	for i := 0; i < lenmy; i++ {
 		sum += A[i]
 	}
-	fmt.Println(sum / float32(lenmy))
+	//fmt.Println(sum / float32(lenmy))
 	result = sum / float32(lenmy)
 	fmt.Println(result)
 	//no change
